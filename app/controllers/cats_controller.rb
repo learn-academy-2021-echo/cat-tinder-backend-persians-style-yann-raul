4a9a607cb09cfb555244c1691b0eb25a42d54033
yannsf29 <yann.crennsf@gmail.com>
2022-02-04 14:50:46 -0800

    api-endpoints

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,35 @@
 class CatsController < ApplicationController
+
+    def index
+        cats = Cat.all
+        render json: cats
+    end
+  
+    def create
+     cat = Cat.create(cat_params)  
+     render json: cat
+    end
+        
+     
+
+  
+    
+        def update
+          
+        end
+
+
+  
+    def destroy
+    end
+     
+    
+    
+    # Handle strong parameters, so we are secure 
+     private
+ def cat_params
+ params.require(:cat).permit(:name, :age, :enjoys, :image)
+    
+
+   end   
 end
