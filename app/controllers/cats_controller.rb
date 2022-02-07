2c25b7c2c098eb1da0c3629a3038cf740571e58e
Echo5Mike <1raul.marquez@gmail.com>
2022-02-07 09:49:29 -0800

    cleaned up code

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,35 +1,24 @@
 class CatsController < ApplicationController
 
     def index
-        cats = Cat.all
-        render json: cats
+    cats = Cat.all
+    render json: cats
     end
   
     def create
-     cat = Cat.create(cat_params)  
-     render json: cat
+    cat = Cat.create(cat_params)  
+    render json: cat
     end
-        
-     
-
   
-    
-        def update
-          
-        end
+    def update   
+    end
 
-
-  
     def destroy
     end
-     
-    
-    
-    # Handle strong parameters, so we are secure 
-     private
- def cat_params
- params.require(:cat).permit(:name, :age, :enjoys, :image)
-    
 
-   end   
+    # Handle strong parameters, so we are secure 
+private
+    def cat_params
+    params.require(:cat).permit(:name, :age, :enjoys, :image)
+    end   
 end 
