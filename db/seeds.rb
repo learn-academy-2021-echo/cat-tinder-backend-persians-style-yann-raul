46be2c8a0d6dbc83f7288d0689fe368a215cf278
yannsf29 <yann.crennsf@gmail.com>
2022-02-08 10:46:31 -0800

    final version

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,22 @@ cats = [
       image: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80'
     },
     {
-      name: 'Jack',
-      age: 5,
-      enjoys: 'Furrrrociously hunting bugs.',
-      image: 'https://images.unsplash.com/photo-1492370284958-c20b15c692d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1049&q=80'
+      name: 'Lola',
+      age: 12,
+      enjoys: 'loves her food more than me.',
+      image: 'https://static.onecms.io/wp-content/uploads/sites/34/2021/09/27/black-cat-kitchen-rug-getty-0921-2000.jpg'
+    },
+    {
+      name: 'Garfield',
+      age: 8,
+      enjoys: 'give me some more lasagna.',
+      image: 'https://www.pluggedin.com/wp-content/uploads/2019/12/garfield-the-movie-1200x720.jpg'
+    },
+    {
+      name: 'Artemus',
+      age: 7,
+      enjoys: 'I can see dead people.',
+      image: 'https://i.ytimg.com/vi/iKA6ZXpGcGQ/maxresdefault.jpg'
     }
   ]
   
